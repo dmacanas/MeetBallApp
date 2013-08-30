fa8bffe244b32fd985976ef64f0c5300651419b8
dmacanas <dominic.macanas@gmail.com>
2013-08-30 14:44:48 -0500

    dm - credential storage

---
+++ MeetBallApp/MBCredentialManager.h
@@ -0,0 +1,13 @@
+//
+//  MBCredentialManager.h
+//  MeetBallApp
+//
+//  Created by Dominic Macanas on 8/30/13.
+//  Copyright (c) 2013 MeetBall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBCredentialManager : NSObject
+
+@end
---
+++ MeetBallApp/MBCredentialManager.m
@@ -0,0 +1,13 @@
+//
+//  MBCredentialManager.m
+//  MeetBallApp
+//
+//  Created by Dominic Macanas on 8/30/13.
+//  Copyright (c) 2013 MeetBall. All rights reserved.
+//
+
+#import "MBCredentialManager.h"
+
+@implementation MBCredentialManager
+
+@end
