ad178bdd9abe6d04ed9654768f9ae31a4be002dc
dmacanas <dominic.macanas@gmail.com>
2013-09-03 17:55:45 -0500

    DM - commenting out properties for now

--- MeetBalls.h
+++ MeetBalls.h
@@ -8,26 +8,25 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
-#import <CoreLocation/CoreLocation.h>
 
 @interface MeetBalls : NSManagedObject
 
-@property (strong, nonatomic) NSString *generalLocationZip;
-@property (strong, nonatomic) NSString *locationNotes;
-@property (assign, nonatomic) BOOL ownerPrivate;
-@property (strong, nonatomic) CLLocation *generalLocationGPX;
-@property (strong, nonatomic) NSString *ownersName;
-@property (assign, nonatomic) NSInteger *meetBallId;
-@property (assign, nonatomic) NSInteger *usageId;
-@property (assign, nonatomic) NSInteger *ownerId;
-@property (strong, nonatomic) NSDate *startDate;
-@property (strong, nonatomic) NSString *generalLocationCity;
-@property (strong, nonatomic) NSString *generalLocationAddress1;
-@property (strong, nonatomic) NSString *meetBallName;
-@property (strong, nonatomic) NSString *meetBallDescription;
-@property (assign, nonatomic) NSInteger *responseId;
-@property (strong, nonatomic) CLLocation *specificLocationGPX;
-@property (strong, nonatomic) NSDate *endDate;
-@property (strong, nonatomic) NSString *locationName;
+//@property (strong, nonatomic) NSString *generalLocationZip;
+//@property (strong, nonatomic) NSString *locationNotes;
+//@property (assign, nonatomic) BOOL ownerPrivate;
+//@property (strong, nonatomic) CLLocation *generalLocationGPX;
+//@property (strong, nonatomic) NSString *ownersName;
+//@property (assign, nonatomic) NSInteger *meetBallId;
+//@property (assign, nonatomic) NSInteger *usageId;
+//@property (assign, nonatomic) NSInteger *ownerId;
+//@property (strong, nonatomic) NSDate *startDate;
+//@property (strong, nonatomic) NSString *generalLocationCity;
+//@property (strong, nonatomic) NSString *generalLocationAddress1;
+//@property (strong, nonatomic) NSString *meetBallName;
+//@property (strong, nonatomic) NSString *meetBallDescription;
+//@property (assign, nonatomic) NSInteger *responseId;
+//@property (strong, nonatomic) CLLocation *specificLocationGPX;
+//@property (strong, nonatomic) NSDate *endDate;
+//@property (strong, nonatomic) NSString *locationName;
 
 @end
